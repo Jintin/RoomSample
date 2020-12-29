84743adc432490aa3f92615d24b0b35684f4cc80
Jintin <jintin.lin1018@gmail.com>
2020-12-29 23:31:35 +0800

    rename

--- app/src/main/java/com/jintin/roomsample/User.kt
+++ app/src/main/java/com/jintin/roomsample/User.kt
@@ -1,10 +1,12 @@
 package com.jintin.roomsample
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "users")
 data class User(
+    @ColumnInfo(name = "user")
     val name: String,
     val age: Int,
     @PrimaryKey(autoGenerate = true)
