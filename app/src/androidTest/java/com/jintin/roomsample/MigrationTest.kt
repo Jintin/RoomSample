d868eac43f7b89763d4d0f12aa27f32d46115596
Jintin <jintin.lin1018@gmail.com>
2021-01-10 22:58:02 +0800

    update

--- app/src/androidTest/java/com/jintin/roomsample/MigrationTest.kt
+++ app/src/androidTest/java/com/jintin/roomsample/MigrationTest.kt
@@ -20,19 +20,19 @@ class MigrationTest {
     @Test
     @Throws(IOException::class)
     fun migrate1To2() {
-        var db = helper.createDatabase(TEST_DB, 1).apply {
-            // db has schema version 1. insert some data using SQL queries.
-            // You cannot use DAO classes because they expect the latest schema.
-            execSQL("CREATE TABLE IF NOT EXISTS `users` (`user` TEXT NOT NULL, `age` INTEGER NOT NULL, `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL)")
+        helper.createDatabase(TEST_DB, 1).apply {
             execSQL("INSERT INTO `users` VALUES ('Peter', '23', '1')")
-            // Prepare for the next version.
             close()
         }
 
-        // Re-open the database with version 2 and provide
-        // MIGRATION_1_2 as the migration process.
-        db = helper.runMigrationsAndValidate(TEST_DB, 2, true, UserDatabase.MIGRATION_1_2)
-        // MigrationTestHelper automatically verifies the schema changes,
-        // but you need to validate that the data was migrated properly.
+        helper.runMigrationsAndValidate(TEST_DB, 2, true, UserDatabase.MIGRATION_1_2).apply {
+            val c = query("SELECT * FROM users WHERE user = 'Peter'")
+            assert(c.count == 1)
+            if (c.moveToNext()) {
+                assert(c.getInt(1) == 23)
+            } else {
+                assert(false)
+            }
+        }
     }
 }
